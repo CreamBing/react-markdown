84a1545944783af552666fc5ff14160065fc4e87
creambing <creambing.zhao@utstarcom.cn>
2020-05-22 18:09:31 +0800

    markdown

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import  ReactMarkdown from 'react-markdown';
+import testMd from './source/Ant Design Pro 初体验.md'
+import CodeBlock from "./md/CodeBlock";
+import HeadingBlock from "./md/HeadingBlock";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ReactMarkdown
+        source={testMd}
+        escapeHtml={false}
+        renderers={{
+          code: CodeBlock,
+          heading: HeadingBlock
+        }}
+      />
     </div>
   );
 }
---
+++ src/source/Ant Design Pro 初体验.md
@@ -0,0 +1,27 @@
+# Ant Design Pro 初体验
+
+## 安装
+
+确保你的 node 以及 npm 环境
+
+国内利用 tyarn 加速，采用淘宝镜像
+
+```shell
+npm install yarn tyarn -g
+```
+
+安装 umi
+
+```shell
+tyarn create umi
+```
+
+选择 `ant-design-pro`
+
+安装依赖以及启动
+
+```shell
+tyarn install
+tyarn start
+```
+
